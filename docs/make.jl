3639bb175f53f3dd9d15d4968d74f7058db10a8a
hondoRandale <jules.rasetaharison@auticon.de>
2021-12-10 18:53:06 +0100

    flush push

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,15 @@
- 
-using Documenter, SimpleFWA
+using Documenter
+using SimpleFWA
 
 makedocs(
-  sitename = "SimpleFWA",
-  format   = Documenter.HTML(),
-  modules  = [SimpleFWA]
+    sitename = "SimpleFWA",
+    format = Documenter.HTML(),
+    modules = [SimpleFWA]
 )
 
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
 deploydocs(
-  repo = "github.com/hondoRandale/SimpleFWA.jl.git"
+    repo = "https://github.com/hondoRandale/SimpleFWA.jl"
 )
