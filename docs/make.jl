b564ac9bfefa163b47d9d78644646382dc5e4465
hondoRandale <jules.rasetaharison@auticon.de>
2021-12-11 05:24:22 +0100

    workflow update3

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [SimpleFWA]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
-    repo = "https://github.com/hondoRandale/SimpleFWA.jl"
+    repo = "git@github.com:hondoRandale/SimpleFWA.jl.git"
 )
