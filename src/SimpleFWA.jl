9b39bbcdce3082c74ce3bd3b69bc51b2e3f93304
hondoRandale <jules.rasetaharison@auticon.de>
2021-12-13 00:01:56 +0100

    inline comments updated

--- src/SimpleFWA.jl
+++ src/SimpleFWA.jl
@@ -28,43 +28,35 @@ module SimpleFWA
     y_min;
   end
 
-   @doc """
 
-   simpleFWA( nFireworks::Int,
-              nSparks::Int;
-              λ_0::Float32,
-              ϵ_A::Float32,
-              C_a::Float32,
-              C_r::Float32,
-              lower::Vector{Float32},
-              upper::Vector{Float32},
-              objFunction::Function,
-              XPrimary::Vector{ Matrix{Float32} },
-              yPrimary::Vector{ Matrix{Float32} },
-              maxiter::Int,
-              ϵ_conv::Float32=1f-6 )
 
-    minimize non-linear objective function with upper, lower bound on solution space
 
-    ...
-    # Arguments
-      - `nFireworks::Int`:                    number of fireworks
-      - `nSparks::Int`:                       number sparks per fw
-      - `λ_0::Float32`:                       maximum initial explosion amplitude
-      - `ϵ_A::Float32`:                       smoothing parameter
-      - `C_a::Float32`:                       explosion amplitude upscale   factor
-      - `C_r::Float32`:                       explosion amplitude downscale factor
-      - `lower::Vector{Float32}`:             lower bound solution space
-      - `upper::Vector{Float32}`:             upper bound solution space
-      - `objFunction::Function`:              objective function to be minimized
-      - `XPrimary::Vector{Matrix{Float32}}`:  feature set primary algorithm
-      - `yPrimary::Vector{Matrix{Float32}}`:  target  set primary algorithm
-      - `maxiter::Int`:                       maximum number of iterations
-      - `ϵ_conv::Float32`:                    convergence parameter
-      # Return
-      - `Tuple`:                              best solution, best val.
-      ...
-    """ ->
+    """
+
+        simpleFWA( nFireworks::Int,
+                   nSparks::Int;
+                   λ_0::Float32,
+                   ϵ_A::Float32,
+                   C_a::Float32,
+                   C_r::Float32,
+                   lower::Vector{Float32},
+                   upper::Vector{Float32},
+                   objFunction::Function,
+                   XPrimary::Vector{ Matrix{Float32} },
+                   yPrimary::Vector{ Matrix{Float32} },
+                   maxiter::Int,
+                   ϵ_conv::Float32=1f-6 )
+
+        minimize objective function objFunction, the solution space is limited by lower and upper bound.
+        The optimization algorithm utilized is an simplified version of dynFireWorksAlgorithm. The nFireworks
+        parameter governs the number of fireworks being evaluated in parallel in each iteration.  nSparks is
+        the number of sparks per firework, in remains constant foreach firework. ϵ_A is the smoothing parameter
+        controlling the variance of amplitudes computed foreach fw. C_a ist the upscaling parameter for explosion
+        amplitudes. C_r is the downscaling parameter for explosion amplitudes. XPrimary is the feature set of the
+        primary algorithm to be tuned. yPrimary is the target set of the primary algorithm. maxiter is the maximum
+        number iteraions.  ϵ_conv denotes the convergence parameter.
+
+    """ 
   @views function simpleFWA( nFireworks::Int,
                              nSparks::Int;
                              λ_0::Float32,
