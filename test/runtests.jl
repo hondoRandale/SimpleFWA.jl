c724e28c9551c4a1c8c20d06caab2675e36950b2
hondoRandale <jules.rasetaharison@auticon.de>
2021-12-13 01:45:53 +0100

    tests cleaned up and extended

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,22 @@ using Test
 using SimpleFWA
 
 @testset "SimpleFWA.jl" begin
-  lower    = [ -10.0f0, -10.0f0 ];
-  upper    = [ 10.0f0, 10.0f0 ];
   XPrimary = Vector{ Matrix{Float32} }( undef, 1 );
   yPrimary = Vector{ Matrix{Float32} }( undef, 1 );
+
+  @test Ackley( [ 0.0f0, 0.0f0 ]; XPrimary, yPrimary )              ≈ 0.0f0
+  @test Booth(  [ 1.0f0, 3.0f0 ]; XPrimary, yPrimary )              ≈ 0.0f0
+  @test Rosenbrock( [ 1.0f0, 1.0f0 ]; XPrimary, yPrimary )          ≈ 0.0f0
+  @test Matyas( [ 0.0f0, 0.0f0 ]; XPrimary, yPrimary )              ≈ 0.0f0
+  @test Three_hump_camel( [ 0.0f0, 0.0f0 ]; XPrimary, yPrimary )    ≈ 0.0f0
+  @test Easom( [ π, π ]; XPrimary, yPrimary )                       ≈ -1.0f0
+  @test Sphere( [0.0f0, 0.0f0]; XPrimary, yPrimary )                ≈ 0.0f0
+  @test Schaffer_function_no2( [0.0f0, 0.0f0]; XPrimary, yPrimary ) ≈ 0.0f0
+  @test Bukin_function_no6( [-10.0f0, 1.0f0 ]; XPrimary, yPrimary ) ≈ 0.0f0
+
+  lower    = [ -10.0f0, -10.0f0 ];
+  upper    = [ 10.0f0, 10.0f0 ];
+
   sFWA( objFunction ) = simpleFWA( 16, 16;
                                    λ_0         = 7.95f0,
                                    ϵ_A         = 0.5f-2,
