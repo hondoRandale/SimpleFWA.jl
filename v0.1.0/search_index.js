f3a6989898b8f33d7c8fc3c419d55666ee756107
Documenter.jl <documenter@juliadocs.github.io>
2021-12-14 09:13:32 +0000

    build based on e234d43

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SimpleFWA.jl","page":"SimpleFWA.jl","title":"SimpleFWA.jl","text":"","category":"section"},{"location":"","page":"SimpleFWA.jl","title":"SimpleFWA.jl","text":"Documentation for SimpleFWA.jl","category":"page"}]
+}
